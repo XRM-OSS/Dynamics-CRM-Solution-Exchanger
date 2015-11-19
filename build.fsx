ad060ef4b5ecfda4fe891b233992b5bab020de5f
Florian Kroenert <Florian.Kroenert@kumavision.de>
2015-11-19 09:51:41 +0100

    Creating nuget package now

---
+++ build.fsx
@@ -0,0 +1,116 @@
+#I @"tools\FAKE\tools\"
+#r @"tools\FAKE\tools\FakeLib.dll"
+
+open Fake
+open Fake.AssemblyInfoFile
+open Fake.Git
+open System.IO
+
+let projectName           = "DynamicsCrmSolutionPorter"
+
+//Directories
+let buildDir              = @".\build\"
+let appBuildDir           = buildDir + @"app\"
+
+let deployDir               = @".\Publish\"
+let appdeployDir            = deployDir + @"app\"
+
+let packagesDir             = @".\packages\"
+
+
+let nugetDir = @".\nuget\" 
+let nugetDeployDir = @"\\build\NuGet-Packages"
+
+let mutable version         = "1.0"
+let mutable build           = buildVersion
+let mutable nugetVersion    = ""
+let mutable asmVersion      = ""
+let mutable asmInfoVersion  = ""
+let mutable setupVersion    = ""
+
+let gitbranch = Git.Information.getBranchName "."
+let sha = Git.Information.getCurrentHash()
+
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; deployDir]
+)
+
+Target "RestorePackages" (fun _ ->
+   RestorePackages()
+)
+
+Target "BuildVersions" (fun _ ->
+
+    let safeBuildNumber = if not isLocalBuild then build else "0"
+
+    asmVersion      <- version + "." + safeBuildNumber
+    asmInfoVersion  <- asmVersion + " - " + gitbranch + " - " + sha
+
+    nugetVersion    <- version + "." + safeBuildNumber
+    setupVersion    <- version + "." + safeBuildNumber
+
+    match gitbranch with
+        | "master" -> ()
+        | "develop" -> (nugetVersion <- nugetVersion + " - " + "beta")
+        | _ -> (nugetVersion <- nugetVersion + " - " + gitbranch)
+
+    SetBuildNumber nugetVersion
+)
+Target "AssemblyInfo" (fun _ ->
+    BulkReplaceAssemblyInfoVersions "src/" (fun f ->
+                                              {f with
+                                                  AssemblyVersion = asmVersion
+                                                  AssemblyInformationalVersion = asmInfoVersion})
+)
+
+Target "BuildApp" (fun _->
+    !! @"src\app\**\*.fsproj"
+      |> MSBuildRelease appBuildDir "Build"
+      |> Log "Build - Output: "
+)
+
+Target "CreateNuget" (fun _ ->     
+    CreateDir nugetDir
+
+    "DynamicsCrmSolutionPorter.nuspec"
+          |> NuGet (fun p -> 
+            {p with               
+                Project = projectName
+                Version = nugetVersion
+                NoPackageAnalysis = true                           
+                ToolPath = @".\tools\Nuget\Nuget.exe"                             
+                OutputPath = nugetDir 
+            })
+
+)
+
+Target "PublishNugetToFeed" (fun _ ->
+    XCopy nugetDir nugetDeployDir 
+)
+
+Target "Publish" (fun _ ->
+    CreateDir appdeployDir
+
+    !! (buildDir @@ @"/**/*.* ")
+      -- " *.pdb"
+        |> CopyTo appdeployDir 
+)
+
+Target "Zip" (fun _ ->
+    !! (buildDir @@ @"\**\*.* ")
+        -- " *.zip"
+            |> Zip appBuildDir (deployDir + projectName + version + ".zip")
+)
+
+"Clean"
+  ==> "RestorePackages"
+  ==> "BuildVersions"
+  =?> ("AssemblyInfo", not isLocalBuild )
+  ==> "BuildApp"
+  ==> "Zip"
+  ==> "Publish"
+  ==> "CreateNuget"
+  ==> "PublishNugetToFeed"
+
+
+RunTargetOrDefault "Publish"
