0c630b69fa94682c13b5156a048d525814cfd787
Florian Kroenert <Florian.Kroenert@kumavision.de>
2016-01-22 11:45:26 +0100

    Removed unnecessary package

--- 00_boot.bat
+++ 00_boot.bat
@@ -1,4 +1,3 @@
 @echo off
 cls
 "tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "tools" "-ExcludeVersion"
--- 01_boot_and_build.bat
+++ 01_boot_and_build.bat
@@ -1,5 +1,4 @@
 @echo off
 cls
-"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\nuget\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "tools" "-ExcludeVersion"
+call 00_boot.bat
 call 02_build.bat %1
